6f9c6491da95359ec477e009bfefeb02260a9a46
Aaron Peron <aaron.peron@yahoo.com>
2015-01-29 14:03:17 -0800

    blog 2

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Web Server is Running Locally";
+            require_once("controller/create-db.php  ");
         ?>
     </body>
 </html>

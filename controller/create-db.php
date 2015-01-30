a8d15402b43b5fd9c2fcee0d31ca4b75d4770b6c
Aaron Peron <aaron.peron@yahoo.com>
2015-01-30 14:04:01 -0800

    working

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,13 +1,27 @@
 <?php
-    require_once("../model/database.php");
+    require_once(__DIR__ . "/../model/database.php");
 
     $connection = new mysqli($host, $username, $password);
     
     if($connection->connect_error) {
         die("Error: " . $connection->connect_error);
     }
-    else {
-     echo "Success: " .$connection->host_info;        
+
+    $exists = $connection->select_db($database);
+    
+    if(!$exists) {
+        $query = $connection->query("CREATE DATABASE $database");
+        
+        if($query){
+             echo "Successfully created database: " . $database;
+        }
     }
+    else {
+        echo "Database already exists.";
+    }
+    
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            . "");
 
     $connection->close();
--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            require_once("controller/create-db.php  ");
+            require_once(__DIR__ . "/controller/create-db.php");
         ?>
     </body>
 </html>

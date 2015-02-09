32c67d1ac4427ddbabf95b6d8dc11921ebe0c4f5
Aaron Peron <aaron.peron@yahoo.com>
2015-02-09 14:20:49 -0800

    27

---
+++ model/Database.php
@@ -0,0 +1,13 @@
+<?php
+class Database {
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+    
+    public function __construct() {
+        ;
+    }
+}
+
--- model/config.php
+++ model/config.php
@@ -1,2 +1,7 @@
 <?php
     $path = "/aaron-blog/";
+    
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "blog_db";
